dc3735822a4f1ce3f2d95e59ee955451385990d4
Ion Beristain <jonboyka@gmail.com>
2026-05-03 21:26:18 +0200

    module-infoegokitzea

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module pakag.kudeatzailea {
     requires javafx.controls;
+    requires javafx.swing;
     requires java.sql;
+    requires java.desktop;
     requires jdk.xml.dom;
 
     opens pakag.aplikazioa to javafx.graphics;
